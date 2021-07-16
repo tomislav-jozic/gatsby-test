194fab963e8ad67415ed4c6a781b367674be3db4
Tomislav Jozić <tommy@bb.agency>
2021-07-16 11:41:48 +0200

    change wp url

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         // the only required plugin option for WordPress is the GraphQL url.
         url:
           process.env.WPGRAPHQL_URL ||
-          `http://ref.test/graphql`,
+          `https://refersion.wp.bb.agency/graphql`,
       },
     },
 
